cebdac8b0a573360d28e607ace6494767bfabc6d
PSKuznetsov <paulrussian@me.com>
2015-12-26 00:42:07 +0300

    client

---
+++ C#/ClientServerCL/ClientServerCL/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Parse;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientServerCL
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+ 
+  			ParseClient.Initialize("3TnbmAsf7UxT8Dj5V1nwPClMNmf294E3FzwxC7iM", 
+  									"0u165RJwssOZmZWLbtHiy4j8QXpvvGrgDMRfYAas");
+
+  			User newUser = new User();
+
+  			newUser.cardNumber = 1234123412341234;
+  			newUser.email      = "email@mail.com";
+  			newUser.firstName  = "Petya";
+  			newUser.lastName   = "Ivanov";
+  			newUser.address    = "Saratov, Volskaya street";
+  			newUser.password   = "qwertypass";
+  			newUser.userName   = "IPetya";
+
+  		    //Task<bool> result = newUser.signUpWithPrivacy();
+  		    //result.Wait();
+  			Task<bool> result = newUser.signInWithPrivacy();
+  			result.Wait();
+  			//newUser.logOut();
+  			Console.WriteLine("{0}", newUser.IsUserLoggedIn());
+		}
+	}
+}
---
+++ C#/ClientServerCL/ClientServerCL/User.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Parse;
+using System.Diagnostics;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ClientServerCL
+{
+	public class User
+	{
+
+		public UInt64 cardNumber {set; get;}
+		public string firstName {set; get;}
+		public string lastName {set; get;}
+		public string address {set; get;}
+		public string userName {set; get;}
+		public string password {set; get;}
+		public string email {set; get;}
+
+		public User ()
+		{
+			
+		}
+
+		public async Task<bool> signUpWithPrivacy() {
+			if (firstName != null && lastName != null && userName != null && password != null && email != null) {
+				
+				var newUser = new ParseUser() {
+
+					Username = userName,
+					Password = password,
+					Email    = email
+				};
+
+				newUser["firstName"]  = firstName;
+				newUser["lastName"]   = lastName;
+				newUser["address"]    = address;
+				newUser["cardNumber"] = cardNumber;
+
+				Console.WriteLine("Start authentication!\n");
+
+				try {
+					await newUser.SignUpAsync();
+					Console.WriteLine("User is sign up!\n");
+					return true;
+				}
+				catch (Exception e) {
+					Console.WriteLine(e.ToString());
+					return false;
+				}
+
+			}
+			return false;
+		}
+
+		public async Task<bool> signInWithPrivacy() {
+			if (userName != null && password != null) {
+			Console.WriteLine("Starting Authentication!");
+				try {
+    				await ParseUser.LogInAsync(userName, password);
+    				return true;
+				}
+				catch (Exception e) {
+					Console.WriteLine(e.Message);
+					return false;
+				}
+			}
+
+			return false;
+		}
+
+		public void logOut() {
+			try {
+				ParseUser.LogOut();
+			}
+			catch (Exception e) {
+				Console.WriteLine("{0}", e.ToString());
+			}
+
+		}
+
+		public bool IsUserLoggedIn () {
+  			if (ParseUser.CurrentUser != null) {
+    			return true;
+  			}
+  			else {
+    			return false;
+  			}
+		}
+
+	}
+}
+
