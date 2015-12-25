c884f1ef0ccf05cdeec516f724675d7ec1c38604
PSKuznetsov <paulrussian@me.com>
2015-12-26 00:39:57 +0300

    c# init

---
+++ C#/C#_Practic_4_VI_11/C#_Practic_4_VI_11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace C_Practic_4_VI_11
+{
+	class MainClass
+	{
+		static bool isPrime (int n)
+		{
+			if (n == 1)
+				return false;
+
+			for (int i = 2; i <= Math.Sqrt(n); ++i) {
+				if (n % i == 0) return false;
+			}
+			return true;
+		}
+
+		static void divNumber(int n) {
+			for (int i = 1; i <= n/2; ++i) {
+				if (n % i == 0) {
+					Console.WriteLine("{0} divider for {1}",i,n);
+				}
+			}
+		}	
+			
+		public static void Main (string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			if (isPrime(n)) {
+				Console.WriteLine("Number {0} is a prime number. ", n);
+			}
+			else {
+				divNumber(n);
+			}
+		}
+	}
+}
