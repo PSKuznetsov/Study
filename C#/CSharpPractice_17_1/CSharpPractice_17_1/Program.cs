4fa335789ff9851c977889eaa4d34493b24c306c
PSKuznetsov <paulrussian@me.com>
2015-12-26 00:41:35 +0300

    c#

---
+++ C#/CSharpPractice_17_1/CSharpPractice_17_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpPractice_17_1
+{
+	class MainClass
+	{
+		private static int FindPositionOfDivider(string inputString) {
+            if (inputString.Length > 1) { 
+                int ptr_idx = 0;
+
+                for (int idx = 0; idx < inputString.Length; idx++) {
+
+                    if (inputString[idx] == '(') {
+                        ptr_idx++;
+                    }
+                    else if (inputString[idx] == ')') {
+                        ptr_idx--;
+                    }
+
+                    if (inputString[idx] == ',' && ptr_idx == 1 ) {
+                        return idx;
+                    }
+                }
+            }
+            
+            return -1;
+        }
+
+        private static int StringDivider(string inputString) {
+
+            int idx = FindPositionOfDivider(inputString);
+
+            if (inputString[0] == 'M') {
+                return Math.Max(StringDivider(inputString.Substring(2, idx - 2)), 
+                StringDivider(inputString.Substring(idx + 1, inputString.Length - 2 - idx)));
+           	}
+          	else if (inputString[0] == 'm') {
+                return Math.Min(StringDivider(inputString.Substring(2, idx - 2)), 
+                StringDivider(inputString.Substring(idx + 1, inputString.Length - 2 - idx)));
+            }
+            return int.Parse(inputString);
+        }
+
+		public static void Main (string[] args) {
+
+			StreamReader input  = new StreamReader("formula.txt");
+            StreamWriter output = new StreamWriter("output.txt");
+
+            output.WriteLine(StringDivider(input.ReadLine().Trim()));
+            
+            input.Close();
+            output.Close();
+		}
+	}
+}
+
+
+        
